fad2833436c243c173bd2bdd61d4f8b91e38f6fb
Albert Vesterlund <albert.vesterlund@gmail.com>
2024-12-14 11:39:20 +0100

    Finding A Tree In A Matrix Stack

---
+++ 2015/04_The_Ideal_Stocking_Stuffer/main.py
@@ -0,0 +1,85 @@
+import sys, getopt
+import numpy as np
+import math
+import re
+from enum import Enum
+import copy
+import queue
+np.set_printoptions(threshold=sys.maxsize, linewidth=sys.maxsize)
+
+def readFile(filepath:str):
+    f = open(filepath)
+
+    fileContent = f.read()
+    f.close()
+
+    return fileContent
+
+def parseInput(filePath:str):
+    fileContent = readFile(filePath)
+
+    
+    return fileContent
+
+
+    
+def part1(data:str):
+    
+    import hashlib
+    
+    res = hashlib.md5(data.encode())
+    i = 2470849
+    print(res.hexdigest()[:5])
+    while not all(x==0 for x in res.hexdigest()[:5]):
+        s2h = data + "{}".format(i)
+        res = hashlib.md5(s2h.encode())
+        i+=1
+        
+        print(i, res.hexdigest()[0:5])
+    
+    return i
+
+
+def part2(data):
+    return
+
+    
+
+def main(argv):
+    noPartOne = False
+    noPartTwo = False
+    onlyExample = False
+    argFile = ""
+
+    opts, args = getopt.getopt(argv,"odf:",["no-part-1","no-part-2", "only-example"])
+    for opt, arg in opts:
+        if opt in ("-o", "--only-example"):
+            onlyExample = True
+        elif opt == "--no-part-1":
+            noPartOne = True
+        elif opt == "--no-part-2":
+            noPartTwo = True
+        elif opt in ['-f']:
+            argFile = arg
+    
+    exampleFiles = ["example.txt"]
+    problemFiles = ["input.txt"]
+
+    problemFiles = exampleFiles + problemFiles if not onlyExample else exampleFiles
+
+    if argFile:
+        problemFiles = [argFile]
+
+    for file in problemFiles:
+        data = parseInput(file)
+
+        if not noPartOne:
+            result = part1(copy.deepcopy(data))
+            print("{} - Part 1: {}".format(file, result))
+        
+        if not noPartTwo:
+            result = part2(copy.deepcopy(data))
+            print("{} - Part 2: {}".format(file, result))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
